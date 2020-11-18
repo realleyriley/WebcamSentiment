db853a72911cd4e7eec36c152b53e6fe5828c2db
Riley Tallman <rptallman@gmail.com>
2020-11-17 17:46:49 -0700

    created live video feed using sockets

--- Flask Server/README.md
+++ Flask Server/README.md
@@ -1,4 +1,27 @@
 # Flask Server
 This folder contains necessary files to run the server
 
-Run the server with `python app.py`
+Run the server with `python app.py` inside of a conda environment or with the appropriate packages installed.
+
+
+# Conda env
+In this folder you will find `webcam.yml` which is an Anaconda file. It can automatically create a conda environment with this command: 
+```
+conda env create --file webcam.yml
+```
+
+I have had problems with that in the past though, so you can always create a conda environment by hand using these commands:
+```
+conda create -n webcam python=3.7 flask opencv eventlet
+conda activate webcam
+conda install -c conda-forge imutils
+conda install -c conda-forge flask-socketio
+```
+
+
+# Links
+Tutorial from pytorch about server-side inference:
+https://pytorch.org/tutorials/intermediate/flask_rest_api_tutorial.html
+
+Flask video stream: 
+https://stackoverflow.com/questions/58931854/how-to-stream-live-video-frames-from-client-to-flask-server-and-back-to-the-clie
--- Flask Server/app.py
+++ Flask Server/app.py
@@ -1,29 +1,64 @@
 # Flask Server code
+# adapted from https://stackoverflow.com/questions/58931854/how-to-stream-live-video-frames-from-client-to-flask-server-and-back-to-the-clie
+# and from https://github.com/PrettyPrinted/flask-socketio-chat/blob/master/index.html
+
 
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
+from flask_socketio import SocketIO, emit
+
+# this is useful for handling multiple clients (multiple tabs)
+# lock = threading.Lock()
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
-# this is called when just the root domain is requested, ie https://mydomain.com/
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 # this is the url to be used while predicting the label of the test video
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.data
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     data = request.data
 
-    return json_dict
+#     return json_dict
 
 # We can use this url extension to send files to the server
 # https://mydomain.com/do_something
-@app.route('/do_something', methods=['POST'])
-def upload():
-    file = request.files['myfile']
+# @app.route('/do_something', methods=['POST'])
+# def upload():
+#     file = request.files['myfile']
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template('index.html')
+
+@socketio.on('image')
+def image(data_image):
+    sbuf = StringIO()
+    sbuf.write(data_image)
+
+    # decode and convert into image
+    b = io.BytesIO(base64.b64decode(data_image))
+    pimg = Image.open(b)
+
+    ## converting RGB to BGR, as opencv standards
+    frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+
+    # Process the image frame
+    frame = imutils.resize(frame, width=700)
+    frame = cv2.flip(frame, 1)
+    imgencode = cv2.imencode('.jpg', frame)[1]
+
+    # base64 encode
+    stringData = base64.b64encode(imgencode).decode('utf-8')
+    b64_src = 'data:image/jpg;base64,'
+    stringData = b64_src + stringData
+
+    # emit the frame back
+    emit('response_back', stringData)
 
 
 # if we are running this file directly rather than importing it:
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    # app.run(port=5000, debug=True)
+    socketio.run(app, host='127.0.0.1', debug=True)
